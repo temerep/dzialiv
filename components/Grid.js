e3921dc2de1f6ca85d3b3387612400b860f4370a
temerep <artem.repey@gmail.com>
2023-10-08 10:11:44 +0300

    update

--- components/Grid.js
+++ components/Grid.js
@@ -3,7 +3,7 @@ import { Card } from "./Card"
 const Grid = ({ children }) => {
   return(
     <>
-    <div className="grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-5 p-5">
+    <div className="grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-0 md:gap-5 p-0 md:p-5">
         {children}
     </div>
     </>
