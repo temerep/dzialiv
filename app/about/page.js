d6f7132c767c7dda686d5317b5254e2dee80bbdc
temerep <artem.repey@gmail.com>
2023-10-08 10:22:05 +0300

    update

--- app/about/page.js
+++ app/about/page.js
@@ -25,7 +25,7 @@ export default function About() {
             </div>
             <div className="flex flex-col md:flex-row gap-x-5 mt-5 w-full mb-10 lg:mb-0">
               <Image src="/photos/about_2.webp" width={500} height={500} alt="" className="w-full rounded-xl" draggable={false} />
-              <Image src="/photos/vagy.jpg" width={500} height={500} alt="" className="w-full rounded-xl" draggable={false} />
+              <Image src="/photos/vagy.jpg" width={500} height={500} alt="" className="w-full mt-3 md:mt-0 rounded-xl" draggable={false} />
             </div>
           </div>
         </div>
