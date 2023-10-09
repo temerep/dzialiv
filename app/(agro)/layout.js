6748f9de02272d98cf8c4e7db681aff99fcedb25
temerep <artem.repey@gmail.com>
2023-10-09 17:59:10 +0300

    correct card showing

--- app/(agro)/layout.js
+++ app/(agro)/layout.js
@@ -22,7 +22,9 @@ export default function ProductLayout({ children }) {
               <>
                 <button className="group border-b border-stone-300 bg-white focus:outline-none">
                   <div className="flex items-center justify-between h-12 px-3 font-semibold hover:bg-stone-200 group-focus:bg-stone-200">
-                    <span className="truncate">{el.name}</span>
+                    <Link href={`/${el.category_id}/${el.link}`}>
+                    <span className="truncate cursor-pointer hover:text-emerald-600">{el.name}</span>
+                    </Link>
                     <svg className="h-5 w-5 duration-300 group-focus:rotate-180" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
                       <path fillRule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clipRule="evenodd" />
                     </svg>
